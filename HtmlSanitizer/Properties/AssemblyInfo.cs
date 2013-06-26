cfd7472edfb7cee5e4509c11c9e18a27fc796c8f
Michael Ganss <michael@ganss.org>
2013-06-26 17:05:51 +0200

    - split tests into separate project - changed to class library project

--- HtmlSanitizer/Properties/AssemblyInfo.cs
+++ HtmlSanitizer/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HtmlSanitizer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cleans HTML from constructs that can be used for XSS")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Ganss")]
 [assembly: AssemblyProduct("HtmlSanitizer")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
