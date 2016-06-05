29339514cfc93e98f42a2f0c1335fd54d930115e
Michael Ganss <michael@ganss.org>
2016-06-05 16:54:34 +0200

    Remove info from AssemblyInfo.cs (now in project.json) Fix typo in project.json Remove AssemblyInfo.cs patching from appveyor.yml

--- HtmlSanitizer/Properties/AssemblyInfo.cs
+++ HtmlSanitizer/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("HtmlSanitizer")]
-[assembly: AssemblyDescription("Cleans HTML from constructs that can be used for cross site scripting (XSS)")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Michael Ganss")]
-[assembly: AssemblyProduct("HtmlSanitizer")]
-[assembly: AssemblyCopyright("Copyright © 2013-2016 Michael Ganss")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 #if !NETSTANDARD
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -24,8 +12,3 @@ using System.Runtime.InteropServices;
 [assembly: Guid("16af04e9-e712-417e-b749-c8d10148dda9")]
 
 #endif
-
-// assembly version information will be reset by AppVeyor
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
