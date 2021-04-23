e3505d5f53751fc05c223e568b75ec27c8481820
Michael Ganss <michael@ganss.org>
2021-04-23 18:22:32 +0200

    Remove space

--- test/HtmlSanitizer.Benchmark/HtmlSanitizerBenchmark.cs
+++ test/HtmlSanitizer.Benchmark/HtmlSanitizerBenchmark.cs
@@ -26,7 +26,7 @@ namespace Ganss.XSS.Benchmark
         {
             sanitizer.Sanitize("<p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>\n<p>Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>\n<p>What was most significant about the lunar voyage was not that man set foot on the Moon but that they set eye on the earth.</p>\n<p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>\n<p>For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us.</p>\n");
         }
-        
+
         /// <summary>
         /// Google is script-heavy.
         /// </summary>
