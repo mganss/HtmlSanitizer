c066b4fe89b0cf57999800fb59681b88285f5f97
Michael Ganss <michael@ganss.org>
2025-04-20 20:18:09 +0200

    Target net462 (fixes #573) Implement VS suggestions

--- src/HtmlSanitizer/HtmlSanitizer.cs
+++ src/HtmlSanitizer/HtmlSanitizer.cs
@@ -559,7 +559,7 @@ public class HtmlSanitizer : IHtmlSanitizer
                         foreach (var removedClass in removedClasses)
                             RemoveCssClass(tag, removedClass, RemoveReason.NotAllowedCssClass);
 
-                        if (!tag.ClassList.Any())
+                        if (tag.ClassList.Length == 0)
                             RemoveAttribute(tag, attribute, RemoveReason.ClassAttributeEmpty);
                     }
                     else if (!oldStyleEmpty && attribute.Name == StyleAttributeName && string.IsNullOrEmpty(attribute.Value))
@@ -658,7 +658,7 @@ public class HtmlSanitizer : IHtmlSanitizer
                 {
                     var e = new PostProcessNodeEventArgs(dom, node);
                     OnPostProcessNode(e);
-                    if (e.ReplacementNodes.Any())
+                    if (e.ReplacementNodes.Count != 0)
                     {
                         ((IChildNode)node).Replace([.. e.ReplacementNodes]);
                     }
